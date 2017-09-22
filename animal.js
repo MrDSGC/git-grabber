05024f0b13dfd70d13456c616dcd3c1a27cb10ac
MrDSGC <dcheng47@gmail.com>
2017-09-22 15:41:48 -0700

    practice

--- animal.js
+++ animal.js
@@ -20,28 +20,3 @@ fs.readFile('./animals.txt', 'utf-8', (err, data) => {
   //   console.log(`successfully created ${letter}_animals.txt`)
   // })
 })
-
-// function selectAnimals(animalString, animalLetter) {
-//   return animalString
-//     .split('\n')
-//     .filter(animal => animal.startsWith(animalLetter))
-//     .join('\n')
-// }
-//
-// const animalLetter = process.argv[2].toUpperCase()
-//
-// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     console.log(err)
-//     return
-//   }
-//   const animals = selectAnimals(data, animalLetter)
-//
-//   fs.writeFile(`${animalLetter}_animals.txt`, animals, err => {
-//     if (err) {
-//       console.log(err)
-//       return
-//     }
-//     console.log(`successfully created ${animalLetter}_animals.txt`)
-//   })
-// })

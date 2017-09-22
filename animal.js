5dbde3346e1f67e38a17e0d973bfcfd492810eea
MrDSGC <dcheng47@gmail.com>
2017-09-21 17:11:55 -0700

    notes 1

--- animal.js
+++ animal.js
@@ -1,9 +1,47 @@
 const fs = require('fs');
 
-fs.readFile('./animals.txt', (err, data) => {
+function filterAnimals( animals, letter) {
+  return animals.split("/n").filter(animal => animal.startsWith(letter)).join('/n')
+}
+const letter = process.argv[2].toUpperCase()
+
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(data);
+  const animals = filterAnimals(data, letter)
+  console.log(animals);
+  // fs.writeFile(`${letter}_animals.txt`, animals, err => {
+  //   if(err) {
+  //     console.log(err)
+  //     return
+  //   }
+  //   console.log(`successfully created ${letter}_animals.txt`)
+  // })
 })
+
+// function selectAnimals(animalString, animalLetter) {
+//   return animalString
+//     .split('\n')
+//     .filter(animal => animal.startsWith(animalLetter))
+//     .join('\n')
+// }
+//
+// const animalLetter = process.argv[2].toUpperCase()
+//
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+//   const animals = selectAnimals(data, animalLetter)
+//
+//   fs.writeFile(`${animalLetter}_animals.txt`, animals, err => {
+//     if (err) {
+//       console.log(err)
+//       return
+//     }
+//     console.log(`successfully created ${animalLetter}_animals.txt`)
+//   })
+// })

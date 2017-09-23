15225386a598a64d8ab7045ec57814c025a6f4dc
MrDSGC <dcheng47@gmail.com>
2017-09-23 16:04:08 -0700

    animal file producer

--- animal.js
+++ animal.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 
-function filterAnimals( animals, letter) {
-  return animals.split("/n").filter(animal => animal.startsWith(letter)).join('/n')
+function filterAnimals(animals, letter) {
+  filtered = animals.split('\n').filter(animal => animal.startsWith("C"))
+  return filtered.join('\n')
 }
+
 const letter = process.argv[2].toUpperCase()
 
 fs.readFile('./animals.txt', 'utf-8', (err, data) => {
@@ -12,11 +14,11 @@ fs.readFile('./animals.txt', 'utf-8', (err, data) => {
   }
   const animals = filterAnimals(data, letter)
   console.log(animals);
-  // fs.writeFile(`${letter}_animals.txt`, animals, err => {
-  //   if(err) {
-  //     console.log(err)
-  //     return
-  //   }
-  //   console.log(`successfully created ${letter}_animals.txt`)
-  // })
+  fs.writeFile(`${letter}_animals.txt`, animals, err => {
+    if(err) {
+      console.log(err)
+      return
+    }
+    console.log(`successfully created ${letter}_animals.txt`)
+  })
 })
